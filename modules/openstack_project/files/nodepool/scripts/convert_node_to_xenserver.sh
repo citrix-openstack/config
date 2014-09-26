e8b2ad468acd396438be30d2fa600d6fa525ca36
Mate Lakat <mlakat@mlakat.hu>
2014-09-26 13:00:27 +0200

    Fix convert_node_to_xenserver.sh
    After a1f2a62ba994d8857122329b4bd037b0924f7bd6 the conversion scripts
    not worked, because the bootmenu's entry became `xe` instead of
    `xe-serial`. It broke the whole conversion. This fix adopts to that
    change, and expects `xe` as the menu label.

--- modules/openstack_project/files/nodepool/scripts/convert_node_to_xenserver.sh
+++ modules/openstack_project/files/nodepool/scripts/convert_node_to_xenserver.sh
@@ -403,7 +403,9 @@ function start_ubuntu_on_next_boot() {
 
     bootfiles="$1"
 
-    sed -ie 's,default xe-serial,default ubuntu,g' $bootfiles/extlinux.conf
+    sed -ie 's,default xe,default ubuntu,g' $bootfiles/extlinux.conf
+
+    log_extlinux $bootfiles/extlinux.conf
 }
 
 function start_xenserver_on_next_boot() {
@@ -411,7 +413,18 @@ function start_xenserver_on_next_boot() {
 
     bootfiles="$1"
 
-    sed -ie 's,default ubuntu,default xe-serial,g' $bootfiles/extlinux.conf
+    sed -ie 's,default ubuntu,default xe,g' $bootfiles/extlinux.conf
+
+    log_extlinux $bootfiles/extlinux.conf
+}
+
+function log_extlinux() {
+    local extlinux_conf
+
+    extlinux_conf="$1"
+
+    echo "ACTUAL STATE OF EXTLINUX IS"
+    cat $extlinux_conf
 }
 
 function mount_dom0_fs() {
